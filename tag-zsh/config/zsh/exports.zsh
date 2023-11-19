f7b8ee2505c211af16f131505e44d3d97eb4af18
TwelveNight <1315640046@qq.com>
2023-11-19 13:35:51 +0800

    feat:use nvim as zsh default editor

--- tag-zsh/config/zsh/exports.zsh
+++ tag-zsh/config/zsh/exports.zsh
@@ -3,7 +3,9 @@ HISTSIZE=1000000
 SAVEHIST=1000000
 export TERMINAL="kitty"
 export BROWSER="brave"
+export EDITOR=nvim
 # eval "`pip completion --zsh`"
+
 #zoxide
 eval "$(zoxide init zsh --cmd cd)"
 export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND=(bg=none,fg=magenta,bold)
@@ -13,5 +15,6 @@ export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND=(bg=none,fg=magenta,bold)
 export GOPATH="$HOME/go"
 export GOROOT="$HOME/.go"
 export PATH="$PATH:$GOPATH/bin"
+
 
 
