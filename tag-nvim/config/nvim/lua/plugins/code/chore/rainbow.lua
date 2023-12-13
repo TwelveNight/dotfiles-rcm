777c8e6c9f970c5717d99614dd8f33c0f2e8fb8e
TwelveNight <1315640046@qq.com>
2023-12-13 12:19:05 +0800

    nvim:change rainbow plugin`s path,add surround plugin to rcm

---
+++ tag-nvim/config/nvim/lua/plugins/code/chore/rainbow.lua
@@ -0,0 +1,27 @@
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  -- config = function()
+  --   local rainbow_delimiters = require("rainbow-delimiters")
+  --   strategy = {
+  --     [""] = rainbow_delimiters.strategy["global"],
+  --     vim = rainbow_delimiters.strategy["local"],
+  --   }
+  --   query = {
+  --     [""] = "rainbow-delimiters",
+  --     lua = "rainbow-blocks",
+  --   }
+  --   priority = {
+  --     [""] = 110,
+  --     lua = 210,
+  --   }
+  --   highlight = {
+  --     "RainbowDelimiterRed",
+  --     "RainbowDelimiterYellow",
+  --     "RainbowDelimiterBlue",
+  --     "RainbowDelimiterOrange",
+  --     "RainbowDelimiterGreen",
+  --     "RainbowDelimiterViolet",
+  --     "RainbowDelimiterCyan",
+  --   }
+  -- end,
+}
