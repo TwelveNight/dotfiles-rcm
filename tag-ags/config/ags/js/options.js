8e9bb091df4eaa7b16fd3a1c67c52f318f018c62
TwelveNight <1315640046@qq.com>
2023-12-19 18:12:43 +0800

    ags:add clash to the notifications black_list

--- tag-ags/config/ags/js/options.js
+++ tag-ags/config/ags/js/options.js
@@ -238,7 +238,9 @@ export default {
   },
 
   notifications: {
-    black_list: Option(["Spotify"], { note: "app-name | entry" }),
+    black_list: Option(["Spotify", "Clash"], {
+      note: "app-name | entry",
+    }),
     position: Option(["top"], { note: "anchor" }),
     width: Option(450),
   },
