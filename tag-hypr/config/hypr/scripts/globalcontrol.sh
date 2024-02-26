78ebaacc06a09fc970ead292576f8b8538902437
TwelveNight <1315640046@qq.com>
2024-02-26 21:42:48 +0800

    hyprland:scripts update

--- tag-hypr/config/hypr/scripts/globalcontrol.sh
+++ tag-hypr/config/hypr/scripts/globalcontrol.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 # wallpaper var
-EnableWallDcol=1
+EnableWallDcol=0
 ConfDir="${XDG_CONFIG_HOME:-$HOME/.config}"
-CloneDir="/home/night/hyprland-demo/hyprdots"
+CloneDir="$HOME/Hyprdots"
 ThemeCtl="$ConfDir/hypr/theme.ctl"
 cacheDir="$HOME/.cache/hyprdots"
 
