6174341646796a93a10f2e89f82174e938f551bb
TwelveNight <1315640046@qq.com>
2024-02-26 16:38:35 +0800

    zsh:update env

--- tag-zsh/config/zsh/exports.zsh
+++ tag-zsh/config/zsh/exports.zsh
@@ -18,13 +18,23 @@ export PATH="$PATH:$GOPATH/bin"
 # add bob nvim to path
 export PATH="$PATH:$HOME/.local/share/bob/nvim-bin"
 
+# add cargo to path
+export PATH="$PATH:$HOME/.cargo/bin"
+
 # add toolbox scripts to path
 export PATH="$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts"
 
+
 # add own scripts to path
 export PATH="$PATH:$HOME/.local/scripts"
+
+# disable webkit dmabuf renderer
+export WEBKIT_DISABLE_DMABUF_RENDERER=1
 
 # eval pyenv virtualenv to path
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+# eval starship to path
+eval "$(starship init zsh)"
