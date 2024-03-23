13d524c08d7bd1c785962ab59b2a3ae599d98618
TwelveNight <1315640046@qq.com>
2024-03-23 13:02:13 +0800

    nvim:copilot change

--- tag-nvim/config/nvim/lua/plugins/code/completion/copilot-lua-cmp.lua
+++ tag-nvim/config/nvim/lua/plugins/code/completion/copilot-lua-cmp.lua
@@ -1,85 +1,91 @@
 return {
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   event = "User AstroFile",
-  --   opts = { suggestion = { auto_trigger = true, debounce = 150 } },
-  -- },
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = { "zbirenbaum/copilot.lua" },
-  --   opts = function(_, opts)
-  --     local cmp, copilot = require("cmp"), require("copilot.suggestion")
-  --     local snip_status_ok, luasnip = pcall(require, "luasnip")
-  --     if not snip_status_ok then
-  --       return
-  --     end
-  --     local function has_words_before()
-  --       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  --       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-  --     end
-  --     if not opts.mapping then
-  --       opts.mapping = {}
-  --     end
-  --     opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-  --       if copilot.is_visible() then
-  --         copilot.accept()
-  --       elseif cmp.visible() then
-  --         cmp.select_next_item()
-  --       elseif luasnip.expand_or_jumpable() then
-  --         luasnip.expand_or_jump()
-  --       elseif has_words_before() then
-  --         cmp.complete()
-  --       else
-  --         fallback()
-  --       end
-  --     end, { "i", "s" })
-  --
-  --     opts.mapping["<C-x>"] = cmp.mapping(function()
-  --       if copilot.is_visible() then
-  --         copilot.next()
-  --       end
-  --     end)
-  --
-  --     opts.mapping["<C-z>"] = cmp.mapping(function()
-  --       if copilot.is_visible() then
-  --         copilot.prev()
-  --       end
-  --     end)
-  --
-  --     opts.mapping["<C-right>"] = cmp.mapping(function()
-  --       if copilot.is_visible() then
-  --         copilot.accept_word()
-  --       end
-  --     end)
-  --
-  --     opts.mapping["<C-l>"] = cmp.mapping(function()
-  --       if copilot.is_visible() then
-  --         copilot.accept_word()
-  --       end
-  --     end)
-  --
-  --     opts.mapping["<C-down>"] = cmp.mapping(function()
-  --       if copilot.is_visible() then
-  --         copilot.accept_line()
-  --       end
-  --     end)
-  --
-  --     opts.mapping["<C-j>"] = cmp.mapping(function()
-  --       if copilot.is_visible() then
-  --         copilot.accept_line()
-  --       end
-  --     end)
-  --
-  --     opts.mapping["<C-c>"] = cmp.mapping(function()
-  --       if copilot.is_visible() then
-  --         copilot.dismiss()
-  --       end
-  --     end)
-  --
-  --     return opts
-  --   end,
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "User AstroFile",
+    -- opts = { suggestion = { auto_trigger = true, debounce = 150 } },
+
+    opts = {
+      suggestion = { auto_trigger = true, debounce = 150 },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    opts = function(_, opts)
+      local cmp, copilot = require("cmp"), require("copilot.suggestion")
+
+      cmp.event:on("menu_opened", function()
+        vim.b.copilot_suggestion_hidden = true
+      end)
+
+      cmp.event:on("menu_closed", function()
+        vim.b.copilot_suggestion_hidden = false
+      end)
+
+      if not opts.mapping then
+        opts.mapping = {}
+      end
+      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+        if copilot.is_visible() then
+          copilot.accept()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
+      opts.mapping["<C-x>"] = cmp.mapping(function()
+        if copilot.is_visible() then
+          copilot.next()
+        end
+      end)
+
+      opts.mapping["<C-z>"] = cmp.mapping(function()
+        if copilot.is_visible() then
+          copilot.prev()
+        end
+      end)
+
+      opts.mapping["<C-right>"] = cmp.mapping(function()
+        if copilot.is_visible() then
+          copilot.accept_word()
+        end
+      end)
+
+      opts.mapping["<C-l>"] = cmp.mapping(function()
+        if copilot.is_visible() then
+          copilot.accept()
+        end
+      end)
+
+      opts.mapping["<C-down>"] = cmp.mapping(function()
+        if copilot.is_visible() then
+          copilot.accept_line()
+        end
+      end)
+
+      opts.mapping["<C-j>"] = cmp.mapping(function()
+        if copilot.is_visible() then
+          copilot.accept_line()
+        end
+      end)
+
+      opts.mapping["<C-c>"] = cmp.mapping(function()
+        if copilot.is_visible() then
+          copilot.dismiss()
+        end
+      end)
+
+      return opts
+    end,
+  },
+
   -- {
   --   "nvim-lualine/lualine.nvim",
   --   optional = true,
@@ -99,7 +105,13 @@ return {
   --         return icon .. (status.message or "")
   --       end,
   --       cond = function()
-  --         local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+  --         if not package.loaded["copilot"] then
+  --           return
+  --         end
+  --         local ok, clients = pcall(require("lazyvim.util").lsp.get_clients, { name = "copilot", bufnr = 0 })
+  --         if not ok then
+  --           return false
+  --         end
   --         return ok and #clients > 0
   --       end,
   --       color = function()
@@ -112,7 +124,8 @@ return {
   --     })
   --   end,
   -- },
-  -- -- copilot cmp source
+
+  -- copilot cmp source
   -- {
   --   "nvim-cmp",
   --   dependencies = {
@@ -125,7 +138,7 @@ return {
   --         copilot_cmp.setup(opts)
   --         -- attach cmp source whenever copilot attaches
   --         -- fixes lazy-loading issues with the copilot cmp source
-  --         require("lazyvim.util").on_attach(function(client)
+  --         require("lazyvim.util").lsp.on_attach(function(client)
   --           if client.name == "copilot" then
   --             copilot_cmp._on_insert_enter({})
   --           end
